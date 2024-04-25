347bfb0fd43733613027198517a98b2053c0b8e1
guidoffm <guidoffm@users.noreply.github.com>
2024-04-25 23:34:45 +0200

    Introduce env vars in auth config

--- src/app/auth/auth.config.ts
+++ src/app/auth/auth.config.ts
@@ -2,11 +2,11 @@ import { PassedInitialConfig } from 'angular-auth-oidc-client';
 
 export const authConfig: PassedInitialConfig = {
   config: {
-    authority: 'https://idsvr4.azurewebsites.net',
+    authority: process.env['AUTHORITY'] || 'https://idsvr4.azurewebsites.net',
     redirectUrl: window.location.origin + '/callback',
     postLogoutRedirectUri: window.location.origin,
-    clientId: 'spa',
-    scope: 'openid profile offline_access', // 'openid profile offline_access ' + your scopes
+    clientId: process.env['CLIENT_ID'] || 'spa',
+    scope: process.env['SCOPE'] || 'openid profile offline_access', // 'openid profile offline_access ' + your scopes
     responseType: 'code',
     silentRenew: true,
     useRefreshToken: true,
