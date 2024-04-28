e7ac5996823e33c03f28d81b28acf3a12ba86ab6
guidoffm <guidoffm@users.noreply.github.com>
2024-04-28 23:46:41 +0200

    Update foo comp

--- src/app/foo/foo.component.ts
+++ src/app/foo/foo.component.ts
@@ -1,12 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-foo',
   standalone: true,
-  imports: [],
+  imports: [
+    
+  ],
   templateUrl: './foo.component.html',
   styleUrl: './foo.component.css'
 })
 export class FooComponent {
+  constructor(private httpClient: HttpClient) {}
 
+  callApi() {
+    this.httpClient.get<any>('/api/test').subscribe();
+  }
 }
