b7262c311bd6c7ca337aeb6666786f1218a2c528
guidoffm <guidoffm@users.noreply.github.com>
2024-04-29 17:46:03 +0200

    Move config to env.js

--- src/app/auth/auth.config.ts
+++ src/app/auth/auth.config.ts
@@ -2,11 +2,11 @@ import { LogLevel, PassedInitialConfig } from 'angular-auth-oidc-client';
 
 export const authConfig: PassedInitialConfig = {
   config: [{
-    authority: 'https://idsvr4.azurewebsites.net',
+    authority: (window as any)['env']['oidcAuthority'],
     redirectUrl: window.location.origin,
     postLogoutRedirectUri: window.location.origin,
-    clientId: 'spa',
-    scope: 'openid profile offline_access', // 'openid profile offline_access ' + your scopes
+    clientId: (window as any)['env']['oidcClientId'],
+    scope: (window as any)['env']['oidcScope'], // 'openid profile offline_access ' + your scopes
     responseType: 'code',
     silentRenew: true,
     useRefreshToken: true,
---
+++ src/assets/env.js
@@ -0,0 +1,5 @@
+window['env'] = {
+    oidcAuthority: 'https://idsvr4.azurewebsites.net',
+    oidcClientId: 'spa',
+    oidcScope: 'openid profile offline_access email'
+};
