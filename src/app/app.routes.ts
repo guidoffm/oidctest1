e58f92241f55a6cb3728abcf6a26773beb702581
guidoffm <guidoffm@users.noreply.github.com>
2024-05-09 01:38:35 +0200

    Cleanup app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AutoLoginPartialRoutesGuard } from 'angular-auth-oidc-client';
-import { FooComponent } from './foo/foo.component';
-import { BarComponent } from './bar/bar.component';
 import { ContainerComponent } from './container/container.component';
 import { UsersComponent } from './users/users.component';
 import { InfoComponent } from './info/info.component';
@@ -10,26 +8,15 @@ import { LogoutComponent } from './logout/logout.component';
 import { RadiusComponent } from './radius/radius.component';
 
 export const routes: Routes = [
-    // {
-    //     path: '',
-    //     redirectTo: 'home',
-    //     pathMatch: 'full'
-    // },
     {
         path: '',
         component: HomeComponent,
     },
-    // children: [
     {
         path: 'p',
         component: ContainerComponent,
         canActivate: [AutoLoginPartialRoutesGuard],
         children: [
-            // {
-            //     path: '',
-            //     component: HomeComponent,
-            //     // canActivate: [AutoLoginPartialRoutesGuard]
-            // },
             // {
             //     path: 'foo',
             //     component: FooComponent,
@@ -52,8 +39,6 @@ export const routes: Routes = [
             },
         ]
     },
-    // ]
-    //     },
     {
         path: 'logout',
         component: LogoutComponent,
