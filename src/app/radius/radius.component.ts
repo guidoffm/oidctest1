45aed225275faeb21d12a7df036e560c063f0885
guidoffm <guidoffm@users.noreply.github.com>
2024-05-08 19:34:18 +0200

    Update radius search

--- src/app/radius/radius.component.ts
+++ src/app/radius/radius.component.ts
@@ -4,7 +4,11 @@ import { Component, OnInit } from '@angular/core';
 import { NgbTypeaheadModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
 import { OperatorFunction, ReplaySubject, Subject, debounceTime, distinctUntilChanged, map, pipe, switchMap } from 'rxjs';
-
+export interface RadiusHit {
+  plz: string;
+  ort: string;
+  distance: number;
+}
 @Component({
   selector: 'app-radius',
   standalone: true,
@@ -24,14 +28,16 @@ export class RadiusComponent implements OnInit {
       pipe(map(y => y.map(z => z.plz))).
       subscribe(x => this.zipcodes.next(x));
   }
-  hits = new Subject<any[]>();
+  hits = new ReplaySubject<RadiusHit[]>();
   zipcodes = new ReplaySubject<string[]>();
 
   zip = '';
   radius = 10;
 
   search() {
-    this.httpClient.get<any[]>(`/api/radius/${this.zip}/${this.radius * 1000}`).subscribe(x => this.hits.next(x));
+    this.httpClient.get<RadiusHit[]>(`/api/radius/${this.zip}/${this.radius * 1000}`).
+    pipe(map(x => x.sort((a, b) => a.distance - b.distance))).
+    subscribe(x => this.hits.next(x));
   }
 
   filterZipcodes: OperatorFunction<string, string[]> = pipe(
@@ -42,5 +48,9 @@ export class RadiusComponent implements OnInit {
       map(y => y.sort())
     ))
   );
+
+  get searchDisabled(){
+    return this.zip.length !== 5 || this.radius < 1 || this.radius > 100;
+  }
 }
 
