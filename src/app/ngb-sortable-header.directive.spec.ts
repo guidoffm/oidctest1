7ae21b14f18478438c39201ebc9f361d7eac63f6
guidoffm <guidoffm@users.noreply.github.com>
2024-05-08 11:44:42 +0200

    Added sorting to users comp

---
+++ src/app/ngb-sortable-header.directive.spec.ts
@@ -0,0 +1,8 @@
+import { NgbSortableHeaderDirective } from './ngb-sortable-header.directive';
+
+describe('NgbSortableHeaderDirective', () => {
+  it('should create an instance', () => {
+    const directive = new NgbSortableHeaderDirective();
+    expect(directive).toBeTruthy();
+  });
+});
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,25 +1,91 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Component, OnInit, QueryList, ViewChildren } from '@angular/core';
+import { ReplaySubject, Subject, firstValueFrom, map, take } from 'rxjs';
+import { NgbSortableHeader, SortEvent } from '../ngb-sortable-header.directive';
 
+export type FlattenedUser = {
+  key: string;
+  city: string;
+  state: string;
+  personId: string;
+  personOrg: string;
+};
+
+export type User = {
+  key: string;
+  data: {
+    city: string;
+    state: string;
+    person: {
+      id: string;
+      org: string;
+    }
+  }
+};
+
+type UserResponse = {
+  results: User[];
+};
+const compare = (v1: string | number, v2: string | number) => (v1 < v2 ? -1 : v1 > v2 ? 1 : 0);
 @Component({
   selector: 'app-users',
   standalone: true,
   imports: [
-    CommonModule
+    CommonModule,
+    NgbSortableHeader
   ],
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
 })
 export class UsersComponent implements OnInit {
+
   constructor(private httpClient: HttpClient) { }
+
+  @ViewChildren(NgbSortableHeader)
+  headers!: QueryList<NgbSortableHeader>;
+
   ngOnInit() {
     this.callApiUsers();
   }
-  users = new Subject<any>();
+  users = new ReplaySubject<FlattenedUser[]>();
+  sortedUsers = new Subject<FlattenedUser[]>();
 
   callApiUsers() {
-    this.httpClient.get<any>('/api/users', { responseType: 'json' }).subscribe(x => this.users.next(x.results));
+    this.httpClient.get<UserResponse>('/api/users').
+      subscribe(x => {
+        const flattenedUsers = x.results.map(user => ({
+          key: user.key,
+          city: user.data.city,
+          state: user.data.state,
+          personId: user.data.person.id,
+          personOrg: user.data.person.org
+        }));
+        this.users.next(flattenedUsers);
+        this.sortedUsers.next(flattenedUsers);
+      });
+  }
+
+  async onSort({ column, direction }: SortEvent) {
+    // alert(`Sorting ${column} ${direction}`);
+
+    // resetting other headers
+    for (const header of this.headers) {
+      if (header.sortable !== column) {
+        header.direction = '';
+      }
+    }
+
+    const users = await firstValueFrom(this.users);
+
+    // sorting countries
+    if (direction === '' || column === '') {
+      this.sortedUsers.next(users);
+    } else {
+      this.sortedUsers.next([...users].sort((a, b) => {
+        const res = compare(a[column], b[column]);
+        return direction === 'asc' ? res : -res;
+      }));
+    }
   }
 }
