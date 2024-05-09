40e5a86f3209c05a4be66428312b5377f12546fc
guidoffm <guidoffm@users.noreply.github.com>
2024-05-09 23:01:49 +0200

    Switch login config to dex

--- src/assets/env.js
+++ src/assets/env.js
@@ -1,5 +1,10 @@
+// window['env'] = {
+//     oidcAuthority: 'https://idsvr4.azurewebsites.net',
+//     oidcClientId: 'spa',
+//     oidcScope: 'openid profile offline_access email'
+// };
 window['env'] = {
-    oidcAuthority: 'https://idsvr4.azurewebsites.net',
-    oidcClientId: 'spa',
+    oidcAuthority: 'https://dex.edvmueller.de',
+    oidcClientId: 'www-edvmueller-de',
     oidcScope: 'openid profile offline_access email'
 };
